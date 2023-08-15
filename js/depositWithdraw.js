d81b16b07d44056d9d6c885ccb1f3a79ef9712d6
mehedi-h <mehedihs2015@gmail.com>
2023-08-15 20:28:33 +0600

    modified

--- js/depositWithdraw.js
+++ js/depositWithdraw.js
@@ -1,10 +1,23 @@
 document.getElementById('btn-deposit').addEventListener('click', function(){
     const depositInput = document.getElementById('deposit-input');
     const depositAmount = depositInput.value;
-    // const depositAmountNubmer = parseFloat(depositAmount);
+    const totalDepositAmount = parseFloat(depositAmount);
+
     const depositeBalance = document.getElementById('deposite-balance');
     const totalBalance = depositeBalance.innerText;
-    depositeBalance.innerText = depositAmount;
+    const totalBalanceAmount = parseFloat(totalBalance);
+
+    const stockBalance = document.getElementById('stock-balance')
+    const totalStock = stockBalance.innerText;
+    const stockBalanceAmount = parseFloat(totalStock);
+
+    const currentDeposit = totalBalanceAmount + totalDepositAmount;
+
+    const remainStockBalance = stockBalanceAmount + totalDepositAmount;
+
+    depositeBalance.innerText = currentDeposit;
+    stockBalance.innerText = remainStockBalance;
+    
 
     // console.log();
     depositInput.value = '';
